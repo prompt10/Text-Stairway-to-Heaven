48e7cab9a347915c0a23270f07e5a018c0d329f0
LAPTOP-5K218H50\Supercom - 1 <ritthirach.kongjarit@mail.kmutt.ac.th>
2021-04-24 13:34:10 +0700

    ++Let's get started

---
+++ Text Stairway to Heaven/Text Stairway to Heaven/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Text_Stairway_to_Heaven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
